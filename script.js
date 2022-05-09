548ace764e2e35e297e16cb269d6d9bbd0e4bbe8
GoldenGeneral22 <63856674+GoldenGeneral22@users.noreply.github.com>
2022-05-09 19:57:20 +0200

    Added copying to clipboard and bugfixes

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const message = document.getElementById('message');
 const number = document.getElementById('number');
 const word = document.getElementById('word');
-const key = document.getElementById('key');
+const keyNumber = document.getElementById('keyNumber');
+const keyWord = document.getElementById('keyWord');
 const encryption = document.getElementById('encryption');
 const decryption = document.getElementById('decryption');
 const output = document.getElementById('output');
@@ -31,26 +32,22 @@ function cryptionCheck(cases) {
     }
 }
 
+function copied() {
+    navigator.clipboard.writeText(output.value);
+    //alert("Copied " + output.value + " to clipboard");
+}
+
 function clicked() {
     inputValue = message.value;
-    keyValue = parseInt(key.value);
+    keyValue = parseInt(keyNumber.value);
 
     chars = inputValue.split('');
 
     i = 0;
     chars.forEach(char => {
-        if(char.toUpperCase(i).charCodeAt(0) > 90 || char.toUpperCase(i).charCodeAt(0) < 65)
-        {
-            chars.splice(i, 1);
-        }
-        i++;
-    })
-
-    i = 0;
-    chars.forEach(char => {
         letter = char.toUpperCase(i).charCodeAt(0);
         letter += keyValue * modeMultiplier;
-        if(letter > 90)
+        if(letter > 90 || letter < 65)
         {
             letter -= 26 * modeMultiplier;
         }
